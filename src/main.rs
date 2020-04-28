297c83884e4fc31bae2dd9f352383f64d17908bb
Julien Levesy <jlevesy@gmail.com>
2020-04-29 00:05:06 +0200

    implement print and println! macros

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello from juju-OS {}", "!");
+    println!("Such funny things incoming");
 
     loop {}
 }
