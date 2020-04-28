5327e88916f6a5fdce7ab3e632a9d27128c69888
Julien Levesy <jlevesy@gmail.com>
2020-04-28 21:54:20 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
